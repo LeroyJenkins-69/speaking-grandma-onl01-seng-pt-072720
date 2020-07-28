c31828c08b34f72cdf921e2f36ef37dc2ce8cb28
Kordell Parrish <parrishkordell@gmail.com>
2020-07-28 23:24:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
-  if "#{phrase}" == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif return "HUH?!, SPEAK UP, SONNY!"
+  elsif "HUH?!, SPEAK UP, SONNY!"
   end
 end
