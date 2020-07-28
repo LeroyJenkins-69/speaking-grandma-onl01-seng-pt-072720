51640e2c44c365366181393f676080d6d6699d0a
Kordell Parrish <parrishkordell@gmail.com>
2020-07-28 23:26:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif == phrase = phrase.downcase
+  else phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
-else 
+  elsif phrase == phrase.upcase
+  return 
   end
 end
